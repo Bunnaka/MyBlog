4b29515bc504c9b0cbc903353b54ca53c2ed4008
Kenta Nakabun <k.naka0831@outlook.jp>
2017-11-13 14:13:18 +0000

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108134632) do
+ActiveRecord::Schema.define(version: 20171113141203) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171108134632) do
     t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
